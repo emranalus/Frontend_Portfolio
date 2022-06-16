7942bafde4742225fc979480f50fa9cc747820b6
alusemirhan@hotmail.com <alusemirhan@hotmail.com>
2022-06-16 13:42:46 +0300

    layout shared navbar

---
+++ WebCore5/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCore5.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public IActionResult Referanslar()
+        {
+            return View();
+        }
+
+    }
+}
