e3472cf20835a1d3f0cc2e9929f9ee3ae46e6b89
Emirhan Aluş <alusemirhan@gmail.com>
2022-06-20 18:56:19 +0300

    warming up

---
+++ CoreEmpty6/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreEmpty6.wwwroot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+    }
+}
