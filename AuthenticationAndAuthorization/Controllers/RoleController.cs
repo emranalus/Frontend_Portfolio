f5118711e5f8d11ca1602b0d12cd62e518b057fc
Emirhan Aluş <alusemirhan@gmail.com>
2022-07-19 16:16:42 +0300

    Add RoleManager Edit

---
+++ AuthenticationAndAuthorization/Controllers/RoleController.cs
@@ -0,0 +1,26 @@
+﻿using AuthenticationAndAuthorization.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationAndAuthorization.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Manager")]
+    public class RoleController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RoleController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(roleManager.Roles);
+        }
+    }
+}
